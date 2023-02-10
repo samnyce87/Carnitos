73962b99ce7acaf750dbd66cd059b7402801cc39
samnyce87 <samirmesmoudi@hotmail.com>
2023-02-10 10:02:48 +0100

    code source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import image from './image.png';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Bienvenue chez carnitos</h1>
+      <img src={image} alt="image" />
+    </div>
+  );
+}
+
+export default App;
